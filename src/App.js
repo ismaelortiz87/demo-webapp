6664fb59988ac0dd0d8be55bc4ff211aa8844c77
Angel M. Adames <angelmadames@gmail.com>
2019-12-11 13:24:58 -0400

    Fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Demo <code> WebApp</code> using ReactJS.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/facebook/create-react-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Using create-react-app.
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    message: '',
+    appName: ''
+  };
+
+  async componentDidMount () {
+    await this.getData();
+  };
+
+  async getData() {
+    await fetch("http://localhost:3001/")
+      .then(data => data.json())
+      .then((result) => {
+        this.setState({
+          message: result.message,
+          appName: result.appName,
+        })
+      })
+      .catch(console.log)
+  };
+
+  render() {
+    const { appName, message } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code> { appName }</code> { message }
+          </p>
+          <a
+            className="App-link"
+            href="https://github.com/facebook/create-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Using create-react-app.
+          </a>
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
